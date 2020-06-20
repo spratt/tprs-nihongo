b51b111d14f89a633d2e29ef49ea750a4ef7cc25
Simon Pratt <simon@pr4tt.com>
2020-06-20 19:05:55 -0400

    Add styled components

--- srs-nihongo/src/App.tsx
+++ srs-nihongo/src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+// Create a Title component that'll render an <h1> tag with some styles
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 function App() {
   return (
     <div className="App">
+      <Title>
+        Hello, styled-components!
+      </Title>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
